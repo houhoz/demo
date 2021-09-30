2b0311ba5a48215aa1fc68ba7b9ea438cfb43b35
yazhao.hou <yazhao.hou@upai.com>
2021-09-30 15:08:04 +0800

    📝 add https://github.com/houhoz/VueBeacool.git

---
+++ vue-beacool/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import vHeader from './components/Header/Header'
+export default {
+  name: 'app',
+  components: {
+    vHeader
+  }
+}
+</script>
+
+<style>
+</style>
